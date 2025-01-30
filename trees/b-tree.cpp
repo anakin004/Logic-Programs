fcbe76f895f7d129c846ed962cbb77b805b1a4fd
anakin004 <ryanmangeno@gmail.com>
2025-01-30 19:34:05 +0000

    added binary search

--- trees/b-tree.cpp
+++ trees/b-tree.cpp
@@ -3,11 +3,11 @@
  * B-Tree in C++
 */
 
-
+#include <climits>
 #include <iostream>
 #include <array>
 #include <memory>
-
+#include <algorithm>
 
 // represents t ... t-1 the minimum number of nodes
 // The root node is exemnt from this rule
@@ -37,18 +37,64 @@ struct Node
 	Node(int key)
 	: n(1), keys(), children(), IsLeaf(true)
 	{
+		
 		keys[0] = key;
+		std::sort(keys.begin(), keys.end());
 	}
 
 };
+
+
+bool bsearch(const std::array<int, MAX_KEYS>& nums, int key);
+Node* find(Node* root, int key);
+
 
 
 int main(int argc, char* argv[])
 {
 	
 	Node a(1);
-	for(uint32_t i = 0 ; i < a.n ; i++)
-	       std::cout << a.keys[i] << " ";	
+	find(&a, 1);
 
 	return 0;
+}
+
+bool bsearch(const std::array<int, MAX_KEYS>& nums, int key)
+{
+	uint32_t l = 0;
+	uint32_t r = nums.size();
+
+	while( l <= r )
+	{
+		uint32_t mid = (l+r)/2;
+		if(nums[mid] == key)
+		{
+			return true;	
+		}
+		else if(nums[mid] < key)
+		{
+			l = mid + 1;	
+		}
+		else
+		{
+			r = mid - 1;
+		}
+	}
+
+	return false;
+}
+
+Node* find(Node* root, int key)
+{
+	// to add more
+	bool foundKey = bsearch(root->keys, key);
+	if(foundKey)
+	{
+	       	std::cout << "Found " << key << '\n';
+	}
+	else
+	{
+		std::cout<< key << " Does Not Exist\n";
+	}
+	return root;
 }
